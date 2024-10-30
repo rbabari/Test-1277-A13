9089501d291ff60f4ce6e92c15d0f8e39d93216c
Mourad0906 <m.mouradchikhi@gmail.com>
2024-10-30 14:24:08 -0400

    Creation classe Produit

---
+++ Produit.java
@@ -0,0 +1,13 @@
+public class Produit {
+    private String code;
+    private String name;
+    private String description;
+    private double price;
+
+    public Produit(String code, String name, String description, double price) {
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+}
